63ba03870cd071c2da1d71abc1b5a8c20d3a5a72
iGom-ravi <95466376+iGom-ravi@users.noreply.github.com>
2022-02-24 15:35:52 +0530

    Update TestSrc.groovy

--- src/com/TestSrc.groovy
+++ src/com/TestSrc.groovy
@@ -5,6 +5,22 @@ public class TestSrc{
   print("Inside Const")
  }
   public String display(String name){
-    return "im returing ${name}"
+    if ("${name}" == "Clean")
+                    {
+                     sh "mvn clean"
+                    }
+              else if ("${name}" == "Compile")
+                    {
+                    sh "mvn clean compile"
+                    }
+               else if ("${name}" == "Test")
+                    {
+                    sh "mvn clean test"
+                    }
+                 else if ("${name}" == "Install")
+                    {
+                    sh "mvn clean install"
+                    }
+             return "im returing ${name}"
   }
 }
