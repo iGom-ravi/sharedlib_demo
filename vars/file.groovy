a6a7660241d50772e7d4aee46ec5ddc34abfdacc
iGom-ravi <95466376+iGom-ravi@users.noreply.github.com>
2022-02-24 11:21:40 +0530

    Update file.groovy

--- vars/file.groovy
+++ vars/file.groovy
@@ -3,7 +3,7 @@ import com.*;
 def info(String name){
   TestSrc test= new TestSrc();
   println(test);
-  println(test.displayname(name));
+  println(test.display(name));
   println("Hello ${name}");
   echo "Hi ${name}"
 }
