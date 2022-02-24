87739bbb716e824035f01b9f31c73f0d2cf528b3
iGom-ravi <95466376+iGom-ravi@users.noreply.github.com>
2022-02-24 15:17:52 +0530

    Update file.groovy

--- vars/file.groovy
+++ vars/file.groovy
@@ -6,4 +6,23 @@ def info(String name){
   println(test.display(name));
   println("Hello ${name}");
   echo "Hi ${name}"
+    
+       if ("${name}" == "Clean")
+                    {
+                    sh "mvn clean"
+                    }
+              else if ("${name}" == "Compile")
+                    {
+                    sh "mvn clean compile"
+                    }
+               else if ("${name}" == "Test")
+                    {
+                    sh "mvn clean test"
+                    }
+                 else if ("${name}" == "Install")
+                    {
+                    sh "mvn clean install"
+                    }
+
+
 }
